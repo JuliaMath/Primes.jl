94f5a46576a9fb01053d8aaf47fa877d80740c7e
Oscar Smith <oscardssmith@gmail.com>
2021-02-25 00:29:35 -0600

    Make factor only use 1 rep of `isprime` during trial division

--- src/Primes.jl
+++ src/Primes.jl
@@ -265,9 +265,14 @@ function factor!(n::T, h::AbstractDict{K,Int}) where {T<:Integer,K<:Integer}
                 n = div(n, p)
             end
             n == 1 && return h
-            isprime(n) && (h[n] = 1; return h)
+            if T==BigInt 
+                isprime(n, reps=1) && (h[n] = 1; return h)
+            else
+                isprime(n) && (h[n] = 1; return h)
+            end
         end
     end
+    isprime(n) && h[n]=1; return h
     T <: BigInt || widemul(n - 1, n - 1) ≤ typemax(n) ? pollardfactors!(n, h) : pollardfactors!(widen(n), h)
 end
 
