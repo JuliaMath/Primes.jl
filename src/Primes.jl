67500e886eb66d8fae319a2f7fec133d14d2ec66
fepaul <fepaul@students.uni-mainz.de>
2023-11-15 16:24:47 +0100

    log

--- src/Primes.jl
+++ src/Primes.jl
@@ -156,14 +156,13 @@ end
 """
     isprime(n::Integer) -> Bool
 
-Returns for INT64:  `true` if `n` is prime, and `false` otherwise 
-        for BigInt: `true` if `n` is probably prime, and `false` otherwise (false-positive rate = 0.25^reps with reps=25)
+Returns for values in the range of an INT64 variable:  `true` if `n` is prime, and `false` otherwise 
+        for bigger values: `true` if `n` is probably prime, and `false` otherwise (false-positive rate = 0.25^reps with reps=25 --> considerered safe)
 
     More detailed:
     for even numbers: returns deterministic and correct results
-    for numbers in the INT 64 range: returns deterministic and correct results (by Lookup-tables, trial-division, Miller-Rabin, Lucas-Test)
-    for BigInt: returns probabilistic results by using Miller-Rabin from GNU Multiple Precision Arithmetic Library 
-                with reps=25, false-positive rate = 0.25^reps (considerered safe)
+    for values in the range of an  INT64 variable: returns deterministic and correct results (by Lookup-tables, trial-division, Miller-Rabin, Lucas-Test)
+    for bigger values: returns probabilistic resultsfrom GNU Multiple Precision Arithmetic Library 
     
 ```julia
 julia> isprime(3)
@@ -299,8 +298,7 @@ and `false` if `x` is composite (not prime). The false positive rate is about `0
 `reps = 25` is considered safe for cryptographic applications (Knuth, Seminumerical Algorithms).
 
 is_probably_prime is inherited from the module IntegerMathUtils that provides a wrapper to access
-functionality from the  GNU Multiple Precision Arithmetic Library (GMP) library, which performs a
-- Baillie-PSW probable prime test, then reps-24 Miller-Rabin probabilistic primality tests
+functionality from the  GNU Multiple Precision Arithmetic Library (GMP) library
 ```julia
 julia> isprime(big(3))
 true
